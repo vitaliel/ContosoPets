160708bfda9084791467daaa9498b5125c46fc10
Vitalie Lazu <vitalie.lazu@gmail.com>
2021-09-18 17:30:37 +0300

    Finished unit 5

--- src/ContosoPets.DataAccess/Data/ContosoPetsContext.cs
+++ src/ContosoPets.DataAccess/Data/ContosoPetsContext.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
-// Add the ContosoPets.Domain.Models using statement
+using ContosoPets.Domain.Models;
 
 namespace ContosoPets.DataAccess.Data
 {
-    public partial class ContosoPetsContext : DbContext
+  public partial class ContosoPetsContext : DbContext
+  {
+    public ContosoPetsContext(DbContextOptions<ContosoPetsContext> options)
+        : base(options)
     {
-        public ContosoPetsContext(DbContextOptions<ContosoPetsContext> options)
-            : base(options)
-        {
-        }
-
-        // Add the DbSet<T> properties
     }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductOrder> ProductOrders { get; set; }
+  }
 }
--- src/ContosoPets.DataAccess/Services/OrderService.cs
+++ src/ContosoPets.DataAccess/Services/OrderService.cs
@@ -5,22 +5,113 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ContosoPets.DataAccess.Data;
 using ContosoPets.Domain.DataTransferObjects;
-// Add the ContosoPets.Domain.Models using statement
+using ContosoPets.Domain.Models;
 
 namespace ContosoPets.DataAccess.Services
 {
-    public class OrderService
+  public class OrderService
+  {
+    private readonly ContosoPetsContext _context;
+
+    public OrderService(ContosoPetsContext context)
     {
-        // Add the constructor code
-
-        // Add the GetAll code
-
-        // Add the GetById code
-
-        // Add the Create code
-
-        // Add the SetFulfilled code
-
-        // Add the Delete code
+      _context = context;
     }
+
+    public async Task<List<CustomerOrder>> GetAll()
+    {
+      List<CustomerOrder> orders = await (
+          from o in _context.Orders.AsNoTracking()
+          orderby o.OrderPlaced descending
+          select new CustomerOrder
+          {
+            OrderId = o.Id,
+            CustomerName = $"{o.Customer.LastName}, {o.Customer.FirstName}",
+            OrderFulfilled = o.OrderFullfiled.HasValue ?
+                  o.OrderFullfiled.Value.ToShortDateString() : string.Empty,
+            OrderPlaced = o.OrderPlaced.ToShortDateString(),
+            OrderLineItems = (
+                  from po in o.ProductOrders
+                  select new OrderLineItem
+                  {
+                    ProductQuantity = po.Quantity,
+                    ProductName = po.Product.Name
+                  }
+              )
+          }
+      ).ToListAsync();
+      return orders;
+    }
+
+    private IQueryable<Order> GetOrderById(int id) =>
+        from o in _context.Orders.AsNoTracking()
+        where o.Id == id
+        select o;
+
+    public async Task<CustomerOrder> GetById(int id)
+    {
+      CustomerOrder order = await (
+          from o in GetOrderById(id)
+          select new CustomerOrder
+          {
+            OrderId = o.Id,
+            CustomerName = $"{o.Customer.LastName}, {o.Customer.FirstName}",
+            OrderFulfilled = o.OrderFullfiled.HasValue ?
+                  o.OrderFullfiled.Value.ToShortDateString() : string.Empty,
+            OrderPlaced = o.OrderPlaced.ToShortDateString(),
+            OrderLineItems = (
+                  from po in o.ProductOrders
+                  select new OrderLineItem
+                  {
+                    ProductQuantity = po.Quantity,
+                    ProductName = po.Product.Name
+                  }
+              )
+          }
+      ).FirstOrDefaultAsync();
+
+      return order;
+    }
+
+    public async Task<Order> Create(Order newOrder)
+    {
+      newOrder.OrderPlaced = DateTime.UtcNow;
+
+      _context.Orders.Add(newOrder);
+      await _context.SaveChangesAsync();
+
+      return newOrder;
+    }
+
+    public async Task<bool> Setfulfilled(int id)
+    {
+      bool isFulfilled = false;
+      Order order = await GetOrderById(id).FirstOrDefaultAsync();
+
+      if (order != null)
+      {
+        order.OrderFullfiled = DateTime.UtcNow;
+        _context.Entry(order).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        isFulfilled = true;
+      }
+
+      return isFulfilled;
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+      var isDeleted = false;
+      Order order = await GetOrderById(id).FirstOrDefaultAsync();
+
+      if (order != null)
+      {
+        _context.Remove(order);
+        await _context.SaveChangesAsync();
+        isDeleted = true;
+      }
+
+      return isDeleted;
+    }
+  }
 }
